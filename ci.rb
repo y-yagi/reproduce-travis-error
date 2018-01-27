c2e3cdc9d6f2b24b6a657fd853e92925cdaf4ed6
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2018-01-27 15:46:40 +0900

    First commit

---
+++ ci.rb
@@ -0,0 +1,18 @@
+begin
+  pid = spawn("bundle exec puma -p 4567", pgroup: true)
+
+  start_time = Time.now
+
+  loop do
+    break if system("lsof -i :4567", 1 => File::NULL)
+
+    if Time.now - start_time > 5
+      puts "Timed out after 5 seconds"
+      exit 1
+    end
+  end
+
+  exit 0
+ensure
+  Process.kill("KILL", -pid) if pid
+end
